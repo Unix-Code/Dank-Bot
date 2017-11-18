0c6687d3339ad2174e7d0e90e42479a1c2375eaf
David Malakh <dmalakh@comcast.net>
2017-11-18 03:24:27 -0500

    Added Filtering by URL Entities in Found Status

--- src/main/java/DankBot.java
+++ src/main/java/DankBot.java
@@ -65,7 +65,11 @@ public class DankBot {
             }
 
             for (Status tweet : result.getTweets()) {
-                if (tweet.getText().toLowerCase().contains("dark") && tweet.getURLEntities().length == 0 && tweet.getMediaEntities().length == 0) {
+                if (tweet.getText().toLowerCase().contains("dark") 
+                        && tweet.getURLEntities().length == 0 
+                        && tweet.getMediaEntities().length == 0 
+                        && tweet.getUserMentionEntities().length == 0
+                        && tweet.getURLEntities().length == 0) {
                     someTweets.add(tweet);
                 }
 
@@ -106,20 +110,5 @@ public class DankBot {
         } catch(TwitterException e) {
             System.out.println("Could not send milestone tweet for some reason.");
         }
-    }
-    
-//    private String getOrdinalSuffixOf(int i) {
-//        int j = i % 10,
-//            k = i % 100;
-//        if (j == 1 && k != 11) {
-//            return i + "st";
-//        }
-//        if (j == 2 && k != 12) {
-//            return i + "nd";
-//        }
-//        if (j == 3 && k != 13) {
-//            return i + "rd";
-//        }
-//        return i + "th";
-//    }
+    } 
 }
